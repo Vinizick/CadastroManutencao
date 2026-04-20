620b4db87d68478d1601451a5bdde937bcac437b
Vinizick <vinizick18@gmail.com>
2026-04-20 13:22:19 -0300

    Fix URL and update login to email in script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-const SUPABASE_URL = "https//db.klyzgqrpefuqbwcxdueu.supabase.co";
+const SUPABASE_URL = "https://db.klyzgqrpefuqbwcxdueu.supabase.co";
 const SUPABASE_KEY = "BvfZii2eOsCvnIWY";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -18,29 +17,28 @@ function abrirModal() {
             <button onclick="cadastrar()">Cadastrar</button>
         `;
     } else {
-        document.getElementById("tituloForm").innerText = "Login / Cadastro";
+        document.getElementById("tituloForm").innerText = "Acesso";
 
         botoes.innerHTML = `
-            <button onclick="logar()">Login</button>
+            <button onclick="logar()">Entrar</button>
             <button onclick="cadastrar()">Cadastrar</button>
         `;
     }
 }
 
-
 function fecharModal() {
     document.getElementById("modal").style.display = "none";
 }
 
-
+// CADASTRO
 async function cadastrar() {
     const nome = document.getElementById("nome").value;
-    const login = document.getElementById("login").value;
+    const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
     const { error } = await client
         .from("seguranca.tbUsuarios")
-        .insert([{ nome, login, senha }]);
+        .insert([{ nome, login: email, senha }]);
 
     if (!error) {
         document.getElementById("msg").innerHTML =
@@ -48,33 +46,29 @@ async function cadastrar() {
 
         localStorage.setItem("primeiroAcesso", "true");
 
-        setTimeout(() => {
-            fecharModal();
-        }, 2000);
+        setTimeout(() => fecharModal(), 2000);
     } else {
         document.getElementById("msg").innerText = error.message;
     }
 }
 
-// login
+// LOGIN
 async function logar() {
-    const login = document.getElementById("login").value;
+    const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
     const { data } = await client
         .from("seguranca.tbUsuarios")
         .select("*")
-        .eq("login", login)
+        .eq("login", email)
         .eq("senha", senha);
 
     if (data && data.length > 0) {
         document.getElementById("msg").innerHTML =
             "<span class='sucesso'>Login efetuado com sucesso!</span>";
 
-        setTimeout(() => {
-            fecharModal();
-        }, 2000);
+        setTimeout(() => fecharModal(), 2000);
     } else {
-        document.getElementById("msg").innerText = "Login inválido";
+        document.getElementById("msg").innerText = "Dados inválidos";
     }
 }
